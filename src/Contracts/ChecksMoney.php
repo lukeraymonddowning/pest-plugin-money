ecef76aed1ba0c0b2c09fce889963c8b3b2ae04f
luke <lukeraymonddowning@gmail.com>
2021-06-20 14:53:00 +0100

    Updates to work with latest version of Pest.

--- src/Contracts/ChecksMoney.php
+++ src/Contracts/ChecksMoney.php
@@ -4,15 +4,41 @@ declare(strict_types=1);
 
 namespace Lukeraymonddowning\PestPluginMoney\Contracts;
 
-use Pest\Expectations\Expectation;
+use Pest\Expectation;
 
+/**
+ * @template TMoney
+ * @phpstan-type MoneyAmount TMoney|string|int|float
+ */
 interface ChecksMoney
 {
+    /**
+     * @template TValue
+     *
+     * @param Expectation<TValue> $expectation
+     *
+     * @return Expectation<TValue>
+     */
     public function toBeMoney($expectation): Expectation;
 
+    /**
+     * @param Expectation $expectation
+     * @param MoneyAmount $amount
+     * @param string      $currency
+     */
     public function toCost($expectation, $amount, $currency): Expectation;
 
+    /**
+     * @param Expectation $expectation
+     * @param MoneyAmount $amount
+     * @param string      $currency
+     */
     public function toCostMoreThan($expectation, $amount, $currency): Expectation;
 
+    /**
+     * @param Expectation $expectation
+     * @param MoneyAmount $amount
+     * @param string      $currency
+     */
     public function toCostLessThan($expectation, $amount, $currency): Expectation;
 }
