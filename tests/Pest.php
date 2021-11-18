33438ff9055763bd622182585fd45604cc688770
Luke Downing <lukeraymonddowning@gmail.com>
2021-11-18 11:40:02 +0000

    Fixes 8.0 issues for pipelines

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,4 +4,4 @@ use Lukeraymonddowning\PestPluginMoney\Tests\Archtech\TestCase;
 use Lukeraymonddowning\PestPluginMoney\Tests\Concerns\ProvidesGlobalFunctionAccessors;
 
 uses(ProvidesGlobalFunctionAccessors::class)->in(__DIR__);
-uses(TestCase::class)->in(__DIR__ . '/Archtech');
+uses(TestCase::class)->group('^8.0')->in(__DIR__ . '/Archtech');
